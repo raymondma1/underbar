f80ddf8a46db9350c69aae4a2f2711708c514324
Raymond Ma <raymondxma@gmail.com>
2014-09-14 21:31:05 -0700

    _.map & _.plck fixed

--- src/underbar.js
+++ src/underbar.js
@@ -129,6 +129,11 @@ var _ = {};
     // map() is a useful primitive iteration function that works a lot
     // like each(), but in addition to running the operation on all
     // the members, it also maintains an array of results.
+    var transArray=[];
+    for(var i =0;i<collection.length;i++){
+      transArray.push(iterator(collection[i],i,collection));
+    }
+    return transArray;
   };
 
   /*
